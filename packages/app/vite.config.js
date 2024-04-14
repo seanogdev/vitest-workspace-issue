45b8e28713180711a01b8436dc82a65b1c462366
Sean O'Grady <1761115+seanogdev@users.noreply.github.com>
2024-04-14 16:30:41 +0100

    Vitest leaking example

---
+++ packages/app/vite.config.js
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from "node:url";
+import AutoImport from "unplugin-auto-import/vite";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({ imports: ["vue"], eslintrc: { enabled: true } }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
---
+++ packages/config/sample.test.js
@@ -0,0 +1,7 @@
+import { describe, test, expect } from "vitest";
+
+describe("sample", () => {
+  test("should pass", () => {
+    expect(2 + 3).toBe(5);
+  });
+});
